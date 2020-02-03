6562337a49666cb2339c7bba8604ca8c84b2fd64
ryun3433 <patmos3433@gmail.com>
2020-02-03 23:59:08 +0900

    20200203

--- READEME.md
+++ READEME.md
@@ -1,3 +1,17 @@
 #Ryutube 
 
 cloning youtube with vanilla and node.js
+
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,28 @@
-export const homeController = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("search",  { pageTitle: "Search" });
-export const videos = (req, res) => res.render("videos",  { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("upload",  { pageTitle: "Upload" });
+import {videos} from "../db"
+import routes from "../routes";
+
+export const homeController = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos })
+}
+
+export const search = (req, res) => {
+    const {query: { term : searchingBy }} = req;
+    res.render("search",  { pageTitle: "Search", searchingBy, videos });
+}
+
+
+export const getUpload = (req, res) => res.render("upload",  { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    ///ToDo : Upload and save video
+    res.redirect(routes.videoDetail(324393));
+};
+
 export const videoDetail = (req, res) => res.render("videoDetail",  { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) => res.render("editVideo",  { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.render("deleteVideo",  { pageTitle: "Delete Video" });
